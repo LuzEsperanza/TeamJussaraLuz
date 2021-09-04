2dcbb0b18f2ada9c27ccd1151f8e899a06b57d0f
LuzEsperanza <luzdeesperanzaapolo@gmail.com>
2021-09-03 22:05:49 -0300

    index

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3333;
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+//app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
